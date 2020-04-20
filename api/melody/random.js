b7824c6f8e0c8e35f22a1379e815f0a66f0adbfc
miketreacy <michael.treacy@live.com>
2020-04-19 20:20:49 -0700

    Migrate motivic to ZEIT Now 2.0 platform mono-repo

---
+++ api/melody/random.js
@@ -0,0 +1,447 @@
+// This file exists to facilitate ZEIT NOW 2.0 serverless deployment
+// https://zeit.co/guides/migrate-to-zeit-now
+const {
+    melody: melodyUtils,
+    general: generalUtils,
+    service: serviceUtils,
+} = require('../_utils/Utils.js')
+const { AppError, RequestError } = require('../_utils/Errors.js')
+const { app: config } = require('../_utils/Config.json')
+
+/**
+ *
+ * @param melody
+ * @param octave
+ * @param leap
+ * @returns {{min: number, max: number, exclude: {min: *, max: *}}}
+ * @private
+ */
+function getNoteValRange(melody, octave, leap) {
+    let pitchArr = melody.filter((n) => n.value !== null)
+    let lastPitchVal = pitchArr.length
+        ? pitchArr[pitchArr.length - 1].value
+        : null
+    let octMin = melodyUtils.getNoteValue('c', octave.low)
+    let octMax = melodyUtils.getNoteValue('b', octave.high)
+    let minVal =
+        leap.max && lastPitchVal !== null ? lastPitchVal - leap.max : octMin
+    let maxVal =
+        leap.max && lastPitchVal !== null ? lastPitchVal + leap.max : octMax
+    return {
+        min: Math.max(minVal, octMin),
+        max: Math.min(maxVal, octMax),
+        exclude: {
+            min:
+                leap.min && lastPitchVal !== null
+                    ? lastPitchVal - leap.min - 1
+                    : null,
+            max:
+                leap.min && lastPitchVal !== null
+                    ? lastPitchVal + leap.min - 1
+                    : null,
+        },
+    }
+}
+
+/**
+ *
+ * @param isRest
+ * @param valRange
+ * @param duration
+ * @param melody
+ * @param noteIdx
+ * @returns {*|{value, name, octave, pitch, duration, steps, startingBeat, interval}}
+ * @private
+ */
+function getRandomNote(isRest, valRange, duration, melody, noteIdx) {
+    let noteVal = isRest
+        ? null
+        : generalUtils.getRandomInt(
+              valRange.min,
+              valRange.max,
+              valRange.exclude
+          )
+    return melodyUtils.getNote(noteVal, duration, melody, noteIdx)
+}
+
+/**
+ *
+ * @param threshold
+ * @returns {boolean}
+ * @private
+ */
+function weight(threshold) {
+    let chance = generalUtils.getRandomInt()
+    return chance < threshold
+}
+
+// NOTE: Algorithmic filter sets ratio of pitches to rests at roughly 2:1
+/**
+ *
+ * @param previousNote
+ * @returns {string}
+ * @private
+ */
+function getNoteType(previousNote) {
+    let type = 'pitch'
+    // Preventing consecutive rests.
+    if (previousNote && previousNote.name !== 'rest') {
+        type = weight(config.percentOfRests) ? 'rest' : 'pitch'
+    }
+    return type
+}
+
+/**
+ * Generates a random rhythmic duration within given inclusive bounds.
+ * @param {number} min Minimum note duration in atomic rhythmic units.
+ * @param {number} max Maximum note duration in atomic rhythmic units.
+ * @returns {*}
+ * @private
+ */
+function getRandomDuration(min, max) {
+    // NOTE: the parameter defaults are a conscious decision to normalize note durations
+    // return generalUtils.roundToUnit(generalUtils.getRandomInt(min, max));
+    return generalUtils.getRandomInt(min, max)
+}
+
+/**
+ * Higher-order function that takes a min and max duration and returns a
+ * validator function for iterating over duration values.
+ * @param {number} min Minimum note duration in atomic rhythmic units.
+ * @param {number} max Maximum note duration in atomic rhythmic units.
+ * @returns {function(number): [boolean, Object]}
+ * @private
+ */
+function durationIsValid(min, max) {
+    /**
+     * Determines if a given duration value honors min and max parameters
+     * @param {number} d Duration value to validate
+     * @returns {[boolean, Object]} A pseudo-tuple of "is valid", diffMap
+     */
+    return function isValid(d) {
+        // if either diff is negative, then number is invalid
+        let diff = { min: d - min, max: max - d }
+
+        return [diff.min >= 0 && diff.max >= 0, diff]
+    }
+}
+
+/**
+ * Determines if a given array of durations is valid
+ * @param {[number]} durations Array of numbers representing note durations
+ * in atomic rhythmic units.
+ * @param {number} min Minimum note duration in atomic rhythmic units.
+ * @param {number} max Maximum note duration in atomic rhythmic units.
+ * @param {number} totalUnitsTarget Number of duration units to fill.
+ * @returns {[boolean, number, [boolean]]} A pseudo-tuple of "is valid", the
+ * total unit diff, and a mapped list of "is valid" for each duration
+ * @private
+ */
+function durationSetIsValid(durations, min, max, totalUnitsTarget) {
+    let totalUnits = generalUtils.sum(durations)
+    let totalDiff = totalUnitsTarget - totalUnits
+    let totalResult = !Math.abs(totalDiff)
+    let isDurValid = durationIsValid(min, max)
+    let diffMap = durations.map(isDurValid)
+    let resultSet = generalUtils.unique(diffMap.map(([isValid]) => isValid))
+    let resultSetValid = resultSet[0] && resultSet.length === 1
+    let result = totalResult && resultSetValid
+    return [result, totalDiff, diffMap]
+}
+
+function distributeDurationValues(durations, min, max, totalUnitsTarget) {
+    console.info(`\nBAD PATH A: valid sum, invalid values`)
+    console.info(`durations: ${durations.join(', ')}`)
+
+    // putting the invalid values in front
+    // durations.pop()
+    durations.reverse()
+
+    let validator = durationIsValid(min, max)
+    let validations = durations.map(validator)
+    let balance = totalUnitsTarget - generalUtils.sum(durations)
+
+    for (let [i, [isValid, diff]] of validations.entries()) {
+        console.log(
+            `${i}: valid:${isValid} diffMin:${diff.min} diffMax:${diff.max}`
+        )
+        let dur = durations[i]
+        if (!isValid) {
+            // if number is invalid, correct it
+            let low = diff.min < 0
+            let high = diff.max < 0
+            if (low) {
+                // if number is lower than min, raise it to min
+                durations[i] = dur - diff.min
+                balance += diff.min
+            } else if (high) {
+                // NOTE: in practice this path is not hit
+                // if number is higher than max, lower it to max
+                durations[i] = dur + diff.max
+                balance += diff.max
+            }
+        } else {
+            if (Math.abs(balance)) {
+                // there is a balance to be paid
+
+                // if number is valid, pay the balance with it
+                if (balance > 0 && dur < max) {
+                    // raise a low number
+                    while (durations[i] < max && balance) {
+                        durations[i]++
+                        balance = balance - 1
+                    }
+                } else if (balance < 0 && dur > min) {
+                    // reduce a high number
+                    while (durations[i] > min && balance < 0) {
+                        durations[i] = durations[i] - 1
+                        balance = balance + 1
+                    }
+                }
+            }
+        }
+    }
+    console.info(`durations: ${durations.join(', ')}`)
+    console.info(`total: ${generalUtils.sum(durations)}`)
+    let [areValid] = durationSetIsValid(durations, min, max, totalUnitsTarget)
+    if (areValid) {
+        console.info(`✅ 🎉Validation was successful!!!🎉\n`)
+        return durations
+    } else {
+        // This should NEVER happen
+        console.info(`durations: ${durations.join(', ')}`)
+        console.info(`🤮 EPIC FAIL!!! 🤮: _getRandomDurations()\n`)
+        throw new AppError(
+            'Application failed to make durations conform to min/max parameters. Please try other min/max values.'
+        )
+    }
+}
+
+/**
+ * Ensures that random duration set confirms to parameters
+ * @param {[number]} durations Array of numbers representing note durations
+ * in atomic rhythmic units.
+ * @param {number} min Minimum note duration in atomic rhythmic units.
+ * @param {number} max Maximum note duration in atomic rhythmic units.
+ * @param {number} totalUnitsTarget Number of duration units to fill.
+ * @returns {*}
+ * @private
+ */
+function validateRandomDurations(durations, min, max, totalUnitsTarget) {
+    let [areValid] = durationSetIsValid(durations, min, max, totalUnitsTarget)
+    if (areValid) {
+        console.debug(
+            `✅ _getRandomDurations() succeeded for total:${totalUnitsTarget}, min:${min}, max:${max}\ndurations: ${durations.join(
+                ','
+            )}`
+        )
+        return durations
+    }
+    return distributeDurationValues(durations, min, max, totalUnitsTarget)
+}
+
+/**
+ * Generates a random sequence of rhythmic durations that subdivide a given
+ * number of beats with an optional min and max duration length.
+ * @param {number} totalUnits Number of beats to generate.
+ * @param {number} min Minimum note duration in atomic rhythmic units.
+ * @param {number} max Maximum note duration in atomic rhythmic units.
+ * @returns {number[]} Array of numbers representing note durations
+ * in atomic rhythmic units.
+ * @private
+ */
+function getRandomDurations(totalUnits, min, max) {
+    // if min and max are the same and they cleanly divide into the total
+    if (min === max && totalUnits % min === 0) {
+        let length = totalUnits / min
+        return Array(length).fill(min)
+    }
+    let results = []
+    let unitsLeft = totalUnits
+
+    while (unitsLeft > 0) {
+        let newBeatMax = Math.min(max, unitsLeft)
+        let newBeat = getRandomDuration(min, newBeatMax)
+        results.push(newBeat)
+        unitsLeft = unitsLeft - newBeat
+    }
+    // Make sure the results satisfy the parameters
+    let validatedResults = validateRandomDurations(
+        results,
+        min,
+        max,
+        totalUnits
+    )
+    // shuffle the results to disperse clumps
+    return generalUtils.shuffle(validatedResults)
+}
+
+/**
+ * Generates a random monophonic melody based on given parameters.
+ * @param {string} key First note of scale.
+ * @param {string} mode Note pattern of scale.
+ * @param {number|null} octaveInit Initial octave in melody.
+ * @param {number|null} octaveLow Lowest octave in melody.
+ * @param {number|null} octaveHigh Highest octave in melody.
+ * @param {string} pitchInit Initial pitch in melody.
+ * @param {string} pitchLow Lowest pitch in melody.
+ * @param {string} pitchHigh Highest pitch in melody.
+ * @param {number} leapMin Minimum leap in steps between melody notes.
+ * @param {number} leapMax Maximum leap in steps between melody notes.
+ * @param {number|null} timeSignatureBeat Beats per measure.
+ * @param {number|null} timeSignatureUnit Beat as division of a whole note.
+ * @param {string} tempoType Method of declaring tempo, eg (bpm vs Italian tempo marking).
+ * @param {number|null} tempoUnits Number of tempo units (n/a if using Italian tempo markings).
+ * @param {string} lengthType Type of length declaration (measures vs time).
+ * @param {number} lengthUnits Number of length units.
+ * @param {number} noteDurMin Minimum note duration in rhythmic units.
+ * @param {number} noteDurMax Maximum note duration in rhythmic unite.
+ * @returns {
+ * {id: (*|string), key: (*|string|string|IDBValidKey|IDBKeyRange|((index: number) => (string | null))),
+ * mode: *, tempo: {tempoType: *, tempoUnits: string}, timeSignature: *[],
+ * leap: {leapMin: number | * | ((...values: number[]) => number) | string,
+ * leapMax: number | * | ((...values: number[]) => number) | string}, notes: Array}}
+ */
+function getRandomMelody({
+    key = config.default.key,
+    mode = config.default.mode,
+    octave: {
+        init: octaveInit = config.default.octave.init,
+        low: octaveLow = config.default.octave.low,
+        high: octaveHigh = config.default.octave.high,
+    } = {},
+    //TODO: handle a pitch config param and delimit melody range within it
+    pitch: {
+        init: pitchInit = config.default.pitch.init,
+        low: pitchLow = config.default.pitch.low,
+        high: pitchHigh = config.default.pitch.high,
+    } = {},
+    leap: {
+        min: leapMin = config.default.leap.min,
+        max: leapMax = config.default.leap.max,
+    } = {},
+    timeSignature: [
+        timeSignatureBeat = config.default.timeSignature.beat,
+        timeSignatureUnit = config.default.timeSignature.unit,
+    ] = [],
+    tempo: {
+        type: tempoType = config.default.tempo.type,
+        units: tempoUnits = config.default.tempo.units,
+    } = {},
+    length: {
+        type: lengthType = config.default.length.type,
+        units: lengthUnits = config.default.length.units,
+    } = {},
+    duration: {
+        min: noteDurMin = config.default.noteDuration.min,
+        max: noteDurMax = config.default.noteDuration.max,
+    } = {},
+} = {}) {
+    // Name-spacing nested arguments because nested JS default params are a pain
+    const octave = { init: octaveInit, low: octaveLow, high: octaveHigh }
+    const pitch = { init: pitchInit, low: pitchLow, high: pitchHigh }
+    const leap = { min: leapMin, max: leapMax }
+    const tempo = { type: tempoType, units: tempoUnits }
+    const length = { type: lengthType, units: lengthUnits }
+    const timeSignature = [timeSignatureBeat, timeSignatureUnit]
+    const duration = {
+        min: Math.max(noteDurMin, config.default.noteDuration.min),
+        max: Math.min(noteDurMax, config.default.noteDuration.max),
+    }
+
+    let inValidDurs = []
+    for (let [k, v] of Object.entries(duration)) {
+        let valid = config.noteDurationOptions.includes(v)
+        if (!valid) {
+            inValidDurs.push([k, v])
+        }
+    }
+
+    if (inValidDurs.length) {
+        let msg = `Invalid note duration parameters: ${inValidDurs
+            .map(([k, v]) => `${v} is not a valid value for ${k}`)
+            .join(', ')}`
+        throw new RequestError(msg)
+    }
+
+    let melody = []
+    let measures = melodyUtils.getMelodyLengthInMeasures(
+        length.type,
+        length.units
+    )
+    // TODO: calculate bpm from Italian tempo markings and return both
+    let bpm = melodyUtils.getMelodyTempoInBPM(tempo.type, tempo.units)
+    let motifBeatLength = melodyUtils.getBeatLength(
+        config.rhythmicUnit,
+        measures,
+        timeSignatureBeat,
+        timeSignatureUnit
+    )
+    let durations = getRandomDurations(
+        motifBeatLength,
+        duration.min,
+        duration.max
+    )
+    let initialOctave =
+        typeof octave.init === 'number'
+            ? octave.init
+            : generalUtils.getRandomInt(octave.low, octave.high)
+    let initialValue = pitch.init
+        ? melodyUtils.getNoteValue(pitch.init, initialOctave)
+        : null
+
+    durations.forEach((d, i) => {
+        // Ensuring that rests will never be consecutive.
+        // TODO: this loop is likely the source of the note duration min/max adherence bug
+        let isRest = getNoteType(i > 0 ? melody[i - 1] : null) === 'rest'
+        let valRange = getNoteValRange(melody, octave, leap)
+        let isFirstPitch = !isRest && !melody.some((n) => n.name !== 'rest')
+        let melodyMap = {
+            key,
+            mode,
+            notes: melody,
+        }
+        try {
+            let newNote =
+                isFirstPitch && initialValue
+                    ? melodyUtils.getNote(initialValue, d, melodyMap, i)
+                    : getRandomNote(isRest, valRange, d, melodyMap, i)
+            melody.push(newNote)
+        } catch (err) {
+            // this is likely because the leap min/max params exceed what
+            // is possible within the given octave range
+            throw new RequestError(
+                `Leap range arguments (${leap.min} - ${leap.max}) conflict with octave range arguments (${octave.low} - ${octave.high})`
+            )
+        }
+    })
+    return {
+        // NOTE: returning all parameter-derived properties (even though endpoint already returns request body)
+        // because all arguments are optional and knowledge of default values may be of use to client.
+        id: generalUtils.randomString(16),
+        meta: {
+            key,
+            mode,
+            octave,
+            pitch,
+            leap,
+            timeSignature,
+            tempo,
+            bpm,
+            length,
+            duration,
+        },
+        notes: melody,
+    }
+}
+
+export default (req, res) => {
+    try {
+        const motif = getRandomMelody(req.body)
+        //TODO: return all extraneous payload properties to client for convenience? 🤔
+        res = serviceUtils.decorateResponseWithHeaders(res)
+        res.json(serviceUtils.getResponse(req, motif))
+    } catch (err) {
+        return res.status(err.statusCode || 500).send(err.message)
+    }
+}
---
+++ api/melody/transform.js
@@ -0,0 +1,287 @@
+// This file exists to facilitate ZEIT NOW 2.0 serverless deployment
+// https://zeit.co/guides/migrate-to-zeit-now
+const {
+    melody: melodyUtils,
+    general: generalUtils,
+    service: serviceUtils,
+} = require('../_utils/Utils.js')
+const { RequestError } = require('../_utils/Errors.js')
+
+// Maps string keys to function values for reference by parameter variables in getTransformedMelody
+const functionMap = {
+    transpose,
+    warp,
+    invert,
+    reverse,
+    mensurate,
+    augment,
+    diminish,
+    stagger,
+}
+/**
+ * Transposes melody to another initial pitch.
+ * @param {Object} melody Melody object.
+ * @param {number} distance (signed integer) Transposition distance in half-steps from the leader's initial pitch.
+ * @param {string} [mode] Name of mode to transpose to.
+ * @param {string} [key] Name of key to transpose to.
+ * @returns {Array} Transposed melody set.
+ */
+function transpose(melody, distance, mode, key) {
+    let clone = generalUtils.clone(melody)
+    let cloneNotes = clone.notes
+
+    clone.mode = mode || clone.mode
+    clone.key = key || clone.key
+    clone.notes = []
+    cloneNotes.forEach((note, i) => {
+        let val = note.value !== null ? note.value + distance : note.value
+        clone.notes.push(melodyUtils.getNote(val, note.duration, clone, i))
+    })
+    return clone
+}
+
+/**
+ * Warps a melody's contour by multiplying the notes absolute distance from a given base note.
+ * If no base note is given, the warp is applied relative to melody's mean or median pitch.
+ * @param {Motif} melody Melody object.
+ * @param {number} factor (signed integer OR float)
+ * @param {string} baseNote Name of base note
+ * @param {number|null} baseOctave Octave of base note
+ */
+//TODO: make sure this is working well with negative factors
+// there may be a weird duration bug happening?
+function warp(melody, factor = 1, baseNote = '', baseOctave = null) {
+    let clone = generalUtils.clone(melody)
+    let cloneNotes = clone.notes
+    let baseNoteValue = null
+    if (baseNote) {
+        baseOctave =
+            baseOctave || melodyUtils.getMeanNotePropertyValue(melody, 'octave')
+        baseNoteValue = melodyUtils.getNoteValue(baseNote, baseOctave)
+    } else {
+        // Param default values will not change melody, so just return it now
+        if (factor === 1) {
+            return melody
+        }
+        baseNoteValue = melodyUtils.getMeanNotePropertyValue(melody, 'value')
+    }
+    clone.notes = []
+    cloneNotes.forEach((note, i) => {
+        let isNote = note.value !== null
+        let noteValue = note.value
+        if (isNote) {
+            let distance = note.value - baseNoteValue
+            let newDistance = distance * factor
+            noteValue = baseNoteValue + newDistance
+        }
+        let newNote = melodyUtils.getNote(noteValue, note.duration, clone, i)
+        clone.notes.push(newNote)
+    })
+    return clone
+}
+
+/**
+ * Inverts melody via contrary motion from initial pitch.
+ * @param {Array} melody Set of note maps. (see documentation above).
+ * @returns {Array} Inverted melody set.
+ */
+function invert(melody) {
+    let clone = generalUtils.clone(melody)
+    let cloneNotes = clone.notes
+
+    clone.notes = []
+    cloneNotes.forEach((note, i, arr) => {
+        let steps = note.steps === null ? null : note.steps * -1
+        let val =
+            steps === null
+                ? null
+                : melodyUtils.getInitialPitch(arr).value + steps
+        clone.notes.push(melodyUtils.getNote(val, note.duration, clone, i))
+    })
+    return clone
+}
+
+/**
+ * Retrogrades melody by reversing contour of the pitch, the rhythm, or both.
+ * @param {Array} melody Set of note maps. (see documentation above).
+ * @param {boolean} rhythm The rhythmic durations will be reversed.
+ * @param {boolean} [pitch] The melodic contour will be reversed.
+ * @returns {Array} Retrogradated melody set.
+ */
+function reverse(melody, rhythm, pitch) {
+    let clone = generalUtils.clone(melody)
+    let cloneNotes = clone.notes
+    let retroNotes = generalUtils.reverseArr(cloneNotes)
+
+    clone.notes = []
+    cloneNotes.forEach((note, i) => {
+        let retroNote = retroNotes[i]
+        let isRest = retroNote.value === null
+        let dur = rhythm ? retroNote.duration : note.duration
+        let val = null
+
+        if (!isRest) {
+            val = pitch
+                ? melodyUtils.getNoteValue(retroNote.name, retroNote.octave)
+                : note.value
+        }
+        clone.notes.push(melodyUtils.getNote(val, dur, clone, i))
+    })
+    return clone
+}
+
+// TODO: adjust measure length of mensurated motif to account for duration changes to enable exact overlay on original melody.
+/**
+ * Mensurates melody by tranforming rhythmic duration values.
+ * @param {Array} melody Set of note maps. (see documentation above).
+ * @param {number} factor Mensuration factor to be applied to rhythmic durations.
+ * @returns {Array} Mensurated melody set.
+ */
+function mensurate(melody, factor = 0) {
+    if (factor) {
+        let clone = generalUtils.clone(melody)
+        let cloneNotes = clone.notes
+        clone.notes = []
+        cloneNotes.forEach((note, i) => {
+            clone.notes.push(
+                melodyUtils.getNote(
+                    note.value,
+                    note.duration * factor,
+                    clone,
+                    i
+                )
+            )
+        })
+        return clone
+    } else {
+        return melody
+    }
+}
+
+/**
+ * Mensurates melody by tranforming rhythmic duration values.
+ * @param {Array} melody Set of note maps. (see documentation above).
+ * @param {number} factor Augmentation factor to be applied to rhythmic durations.
+ * @returns {Array} Augmented melody set.
+ */
+function augment(melody, factor) {
+    return mensurate(melody, factor)
+}
+
+/**
+ * Mensurates melody by tranforming rhythmic duration values.
+ * @param {Array} melody Set of note maps. (see documentation above).
+ * @param {number} factor Diminuition factor to be applied to rhythmic durations.
+ * @returns {Array} Diminished melody set.
+ */
+function diminish(melody, factor) {
+    return mensurate(melody, 1 / factor)
+}
+
+/**
+ * Loops through the beats of the melody to find the delay splice point and
+ * if splice point falls in the middle of a note, splits the note accordingly.
+ */
+// TODO: MODULARIZE THIS MESS - FUNCTION TOO LONG!!
+// TODO: Refactor this to be functionally pure/immutable.
+function stagger(melody, delay = 16) {
+    let clone = generalUtils.clone(melody)
+    let cloneNotes = clone.notes
+    let noteIndex = melodyUtils.getNoteIndexAtBeat(cloneNotes, delay)
+    let cleanBreak = cloneNotes[noteIndex].startingBeat === delay + 1
+    let spliceNotes = []
+    clone.notes = []
+    if (cleanBreak) {
+        spliceNotes = cloneNotes.splice(noteIndex)
+    } else {
+        cloneNotes = cloneNotes.reduce((arr, n, i) => {
+            if (i === noteIndex) {
+                arr = arr.concat([
+                    {
+                        value: n.value,
+                        duration: delay - n.startingBeat,
+                    },
+                    {
+                        value: n.value,
+                        duration: n.duration + n.startingBeat - delay,
+                    },
+                ])
+            } else {
+                arr = arr.concat([n])
+            }
+            return arr
+        }, [])
+        spliceNotes = cloneNotes.splice(noteIndex + 1)
+    }
+    cloneNotes = spliceNotes.concat(cloneNotes)
+    cloneNotes.forEach((n, i) =>
+        clone.notes.push(melodyUtils.getNote(n.value, n.duration, clone, i))
+    )
+    return clone
+}
+
+// Takes a melody instance and an array of strings (function names) to apply to the melody in order
+/**
+ * Applies a series of given transformations to a given melody.
+ * @param {Object} melody Melody with a notes array property.
+ * @param {Array} transformations Array of objects.
+ * @returns {*} transformed melody.
+ */
+function getTransformedMelody(melody = {}, transformations = []) {
+    //TODO: only take array of melody.notes as input and disregard extraneous computed/meta properties?
+    //TODO: reverse engineer all computed melody properties after melody.notes have been transformed?
+    const badRequestError = new RequestError('Invalid request body')
+    if (melody.notes && melody.notes.length && transformations.length) {
+        try {
+            return transformations
+                .map((o) => [o.type.toLowerCase(), o.params])
+                .filter(([fn]) => typeof functionMap[fn] === 'function')
+                .reduce(
+                    (motif, [fn, args]) => functionMap[fn](motif, ...args),
+                    melody
+                )
+        } catch (e) {
+            throw new RequestError('Invalid request body')
+        }
+    } else {
+        let reqParams = {
+            melody: melody && melody.notes && melody.notes.length,
+            transformations: transformations && transformations.length,
+        }
+        let message = Object.keys(reqParams).reduce((msg, field) => {
+            msg += reqParams[field] ? '' : `${field} parameter requires length`
+            return msg
+        }, '')
+        throw new RequestError(`Invalid request body: ${message}`)
+    }
+}
+
+// TODO: melodic analyzer examines multiple dimensions of a melody
+// in order to generate an intelligent counter-melody.
+// The algorithm applies randomness to pre-set routines to maintain
+// uniqueness.
+// Melodic Dimensions to be analyzed:
+// 1. Note duration patterns (rhythm): loops through all 64 beats and looks
+// for clusters of rhythmic activity and inactivity to counterpoint.
+// 2. Melodic contour patterns (melody): analyzes steps, skips, and leaps to
+// counterpoint the shape of the melody.
+// 3. Modal interval usage (harmony): looks at patterns of interval use and how
+// best to harmonize them.
+// 4. Add new 'CONTOUR WARP' transformation that takes a base note, a factor, and a melody and then
+// multiplies or divides each melody note's distance from the base note by the given factor.
+// analyze (melody) {
+//
+// }
+
+export default (req, res) => {
+    try {
+        const motif = getTransformedMelody(
+            req.body.melody,
+            req.body.transformations
+        )
+        res = serviceUtils.decorateResponseWithHeaders(res)
+        res.json(serviceUtils.getResponse(req, motif))
+    } catch (err) {
+        return res.status(err.statusCode || 500).send(err.message)
+    }
+}
