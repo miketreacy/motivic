bf001cfb5a063c7da27485cad749e830948e6632
miketreacy <michael.treacy@live.com>
2020-04-29 19:47:59 -0700

    Remove service worker for now

--- public/register-sw.js
+++ public/register-sw.js
@@ -1,5 +1,6 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-    })
-}
+// TODO: turning this off for now to see if its affecting /feedback redirect
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', () => {
+//         navigator.serviceWorker.register('/service-worker.js')
+//     })
+// }
